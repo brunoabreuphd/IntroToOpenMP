fe329ca9171490f46b630cf7edadcdf7279bb444
Bruno Abreu <84105092+babreu-ncsa@users.noreply.github.com>
2022-01-24 11:51:19 -0600

    Update indexgame_omp.cpp

--- Ex2_DataSharing/indexgame/cpp/solution/indexgame_omp.cpp
+++ Ex2_DataSharing/indexgame/cpp/solution/indexgame_omp.cpp
@@ -63,9 +63,7 @@ int main()
     r2 = rand() % int(ORD);
     r3 = rand() % int(ORD);
     r4 = rand() % int(ORD);
-    cout << "i\t"
-         << "j\t"
-         << "M[i][j]" << endl;
+    cout << "i\t" << "j\t" << "M[i][j]" << endl;
     cout << r1 << "\t" << r2 << "\t" << M[r1][r2] << endl;
     cout << r3 << "\t" << r4 << "\t" << M[r3][r4] << endl;
     // print this specific element
@@ -74,9 +72,9 @@ int main()
 
     // now perform some funky matrix shuffling
     startT = omp_get_wtime(); // trigger stopwatch
-#pragma omp parallel
+    #pragma omp parallel
     {
-#pragma omp for private(j, shuff_i, shuff_j) schedule(dynamic)
+        #pragma omp for private(j, shuff_i, shuff_j) schedule(dynamic)
         for (i = 0; i < ORD; i++)
         {
             for (j = 0; j < ORD; j++)
